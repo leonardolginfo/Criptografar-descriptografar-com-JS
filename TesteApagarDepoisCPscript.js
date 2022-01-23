1031187f4e8c5a9ccaef8328361ad89105490958
Leonardo Gonçalves <leonardo@lginfo.com.br>
2022-01-23 16:48:12 -0300

    criptografar e descriptografar ok - falta btnCopiar e validar

---
+++ TesteApagarDepoisCPscript.js
@@ -0,0 +1,38 @@
+var vogais = ["a", "e", "i", "o", "u"];
+
+var cifra = ["enter", "imes", "ai", "ober", "ufat"];
+
+var textoEntrada = "sera";
+var resultadoCripto;
+var saidaDescriptografada;
+
+function criptografar(texto) {
+
+    resultadoCripto = texto.replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat")
+
+    console.log( resultadoCripto);
+    return resultadoCripto;
+
+}
+
+
+
+function descriptografar(texto) {
+
+    saidaDescriptografada = texto.replace(/ai/gi, 'a')
+        .replace(/enter/gi, 'e')
+        .replace(/imes/gi, 'i')
+        .replace(/ober/gi, 'o')
+        .replace(/ufat/gi, 'u')
+    return saidaDescriptografada;
+}
+
+criptografar(textoEntrada);
+console.log("Texto recebido: " + textoEntrada)
+console.log("Texto criptografdo " + resultadoCripto);
+descriptografar(resultadoCripto);
+console.log("Texto descriptografado: " + saidaDescriptografada);
--- js/script.js
+++ js/script.js
@@ -1,21 +1,40 @@
-var vogais = ["a", "e", "i", "o", "u"];
+var entrada = document.querySelector("#texto-entrada");
+var resultado = document.querySelector('#texto-saida');
+var btnCriptografar = document.querySelector('#btn-cripto');
+var btnDescriptografar = document.querySelector('#btn-descripto');
+var copiarTexto = document.querySelector('#btn-copiar');
 
-var cifra = ["enter", "imes", "ai", "ober", "ufat"];
 
-var textoEntrada = "sera";
-var resultadoCripto;
-var saidaDescriptografada;
 
-function criptografar(texto) {
+btnCriptografar.addEventListener('click', (event) => {
 
-    resultadoCripto = texto.replace(/a/gi, 'ai')
-        .replace(/e/gi, 'enter')
-        .replace(/i/gi, 'imes')
-        .replace(/o/gi, 'ober')
-        .replace(/u/gi, 'ufat');
+    event.preventDefault();
+
+    resultado.textContent = criptografar(entrada.value);
+
+    entrada.value = "";
+
+})
+
+btnDescriptografar.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    resultado.textContent = descriptografar(entrada.value);
+    entrada.value = "";
+})
+
+function criptografar(entrada) {
+
+    resultadoCripto = entrada.replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat")
 
     return resultadoCripto;
+
 }
+
 
 function descriptografar(texto) {
 
@@ -27,8 +46,7 @@ function descriptografar(texto) {
     return saidaDescriptografada;
 }
 
-criptografar(textoEntrada);
-console.log("Texto recebido: " + textoEntrada)
-console.log("Texto criptografdo " + resultadoCripto);
-descriptografar(resultadoCripto);
-console.log("Texto descriptografado: " + saidaDescriptografada);
+
+
+
+    
